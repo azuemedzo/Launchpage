7fe19a7897e81aa034eca866d6c0fedb442668b1
azuemedzo <steeazue@gmail.com>
2025-05-24 12:32:50 -0700

    structure admin

---
+++ app/admin/inbox/page.tsx
@@ -0,0 +1,9 @@
+export default function Page() {
+  return (
+    <div>
+      <h1>Inbox</h1>
+      <p>This is the inbox page.</p>
+      {/* Add your inbox content here */}
+    </div>
+  );
+}
---
+++ app/admin/user/page.tsx
@@ -0,0 +1,9 @@
+export default function Page() {
+  return (
+    <div>
+      <h1>user</h1>
+      <p>This is the user page.</p>
+      {/* Add your user content here */}
+    </div>
+  );
+}
