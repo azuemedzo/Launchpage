fac47c49d3583262725cee62b39bfdb1b7fb710a
azuemedzo <steeazue@gmail.com>
2025-05-24 10:03:56 -0700

    structure

---
+++ app/(Landingpage)/layout.tsx
@@ -0,0 +1,25 @@
+
+import Header from "@/components/header";
+import Footer from"@/components/footer";
+
+
+
+
+
+export default function RootLayout({
+  children,
+}: Readonly<{ 
+  children: React.ReactNode;
+}>) {
+  return (
+   <main>
+
+
+        <Header/>
+        {children}
+        <Footer/>
+   </main>
+    
+  );
+}
+
